0a59ed286628db2e139afd592fac3af9063c4f2b
umavanmoriel <umeda19971102@gmail.com>
2026-04-29 17:59:19 +0200

    --Skapar mapp image i public mappen och lägger till bild för error --Skapar komponent för not-found sida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {Startpage} from './startpage/startpage';
+import {NotFound} from './not-found/not-found';
 
 export const routes: Routes = [
     { path: 'startpage', component: Startpage },
-    { path: '', redirectTo: '/startpage', pathMatch: 'full'}
+    { path: '', redirectTo: '/startpage', pathMatch: 'full'},
+    { path: 'not-found', component : NotFound },
+    { path: '**', component : NotFound}
 ];
---
+++ src/app/not-found/not-found.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.css',
+})
+export class NotFound {
+  notFoundImage : string = "/images/404-error-svgrepo-com.svg";
+}
